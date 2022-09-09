920b0cf5d35b0233efa0ae98821af77dbe99305a
prateekch33 <prateekchandra27@outlook.com>
2022-09-09 22:59:36 +0530

    Update

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,14 +1,14 @@
-const multer=require('multer');
+const multer = require("multer");
 
 var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'routes/uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
+  destination: (req, file, cb) => {
+    cb(null, "routes/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
 });
-  
+
 const upload = multer({ storage: storage });
 
-module.exports=upload;
+module.exports = upload;
